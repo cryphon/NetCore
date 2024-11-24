7747581e1954a6e358e0b4d5c02371f0f898dba0
cryphon <yvanroes@gmail.com>
2024-11-25 02:15:46 +0900

    added simple receivequeue loop for packets

--- src/main.c
+++ src/main.c
@@ -1,13 +1,34 @@
 #include "sysimp.h"
 #include "tuntap.h"
 #include "vn_device.h"
+#include <pthread.h>
 
 #define BUFLEN 2048
+static pthread_t threads[4];
+
+
+static void create_thread(pthread_t id, void *(*func) (void *))
+{
+    if (pthread_create(&threads[id], NULL,
+                       func, NULL) != 0) {
+        perror("Could not create core thread\n");
+    }
+}
 
 int main(void) {
 
     tun_init();
     vn_device_init();
+
+    create_thread(0, vn_device_recqueue_loop);
+
+
+    for (int i = 0; i < 3; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("Error when joining threads\n");
+            exit(1);
+        }
+    }
 
     return 0;
 }
--- src/vn_device.c
+++ src/vn_device.c
@@ -1,13 +1,14 @@
 #include "sysimp.h"
 #include "vn_device.h"
+#include "tuntap.h"
 #include "utils.h"
 
 struct vn_device* loop;
 struct vn_device* vn_device;
 
-static struct vn_device* vn_device_alloc(char* addr, char* hwaddr, uint32_t mtu) {
+static struct vn_device* vn_device_alloc(char* name, char* addr, char* hwaddr, uint32_t mtu) {
     struct vn_device* dev = malloc(sizeof(struct vn_device));
-
+    strncpy(dev->name, name, 15);
     dev->ip_addr = parse_ip(addr); 
 
     sscanf(hwaddr, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &dev->hw_addr[0],
@@ -23,7 +24,22 @@ static struct vn_device* vn_device_alloc(char* addr, char* hwaddr, uint32_t mtu)
 }
 
 void vn_device_init() {
-    loop = vn_device_alloc("127.0.0.1", "00:00:00:00:00:00", 1500);
-    vn_device = vn_device_alloc("10.0.0.4", "00:0c:6d:50:25", 1500);
+    loop = vn_device_alloc("loop", "127.0.0.1", "00:00:00:00:00:00", 1500);
+    vn_device = vn_device_alloc("testtest", "10.0.0.4", "00:0c:6d:50:25", 1500);
 }
+
+void* vn_device_recqueue_loop() {
+    char buffer[2048];
+
+    printf("listening on device %s\n", vn_device->name);
+    while (1) {
+        int len = tun_read(buffer, sizeof(buffer));
+        if (len < 0) {
+            perror("Error reading from TUN device");
+            continue;
+        }
+
+        printf("Received packet\n");
+    }
+    return NULL;}
 
